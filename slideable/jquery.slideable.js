0434ea4bfca717ab5e30687faca1a99d4b879929
Denis Barushev <barushev@gmail.com>
2008-11-10 12:36:07 +0200

    Implemented basic functions of slideable

--- slideable/jquery.slideable.js
+++ slideable/jquery.slideable.js
@@ -5,24 +5,96 @@
 (function ($) {
     $.fn.slideable = function (settings) {
         settings = $.extend({
-            itemWidth: 100
+            debug: false,
+            vertical: false,
+            stepSize: false
         }, settings);
+
+        function debug() {
+            if (settings.debug) {
+                console.log.apply(this, arguments);
+            }
+        }
 
         return this.each(
             function () {
                 var $this = $(this);
 
-                $this.find('.back').click(
-                    function () {
-                        $this.find('.window ul').animate({ marginLeft: -107 });
+                // Set size of the step
+                if (!settings.stepSize) {
+                    if (settings.vertical) {
+                        settings.stepSize = $this.find('li').height();
+                    } else {
+                        settings.stepSize = $this.find('li').width();
                     }
-                );
+                }
 
-                $this.find('.forward').click(
-                    function () {
-                        $this.find('.window ul').animate({ marginLeft: 0 });
+                debug('Step size: ', settings.stepSize);
+
+                function slide() {
+                    if (settings.vertical) {
+                        elWindowUl.animate({ marginTop: ulOffset });
+                    } else {
+                        elWindowUl.animate({ marginLeft: ulOffset });
                     }
-                );
+
+                    checkBackForward();
+                    debug('Ul offset: ', ulOffset);
+                }
+
+                function slideBack() {
+                    if (!elBack.hasClass('disabled')) {
+                        ulOffset += settings.stepSize;
+                        slide();
+                    }
+                }
+
+                function slideForward() {
+                    if (!elForward.hasClass('disabled')) {
+                        ulOffset -= settings.stepSize;
+                        slide();
+                    }
+                }
+
+                function checkBackForward() {
+                    if (ulOffset == 0) {
+                        elBack.addClass('disabled back_disabled');
+                    } else {
+                        elBack.removeClass('disabled back_disabled');
+                    }
+
+                    if (settings.vertical) {
+                        var canSlideForward = (ulOffset - elWindow.height() + elWindowUl.height()) > settings.stepSize;
+                    } else {
+                        var canSlideForward = (ulOffset - elWindow.width() + elWindowUl.width()) > settings.stepSize;
+                    }
+
+                    if (canSlideForward) {
+                        elForward.removeClass('disabled forward_disabled');
+                    } else {
+                        elForward.addClass('disabled forward_disabled');
+                    }
+                }
+
+                var elWindow   = $this.find('.window');
+                var elWindowUl = $this.find('.window ul');
+
+                var elBack    = $this.find('.back');
+                var elForward = $this.find('.forward');
+
+                if (settings.vertical) {
+                    var ulOffset = parseInt(elWindowUl.css('margin-top'));
+                } else {
+                    var ulOffset = parseInt(elWindowUl.css('margin-left'));
+                }
+
+                // Handle click on .back
+                elBack.bind('click', slideBack);
+
+                // Handle click on .forward
+                elForward.bind('click', slideForward);
+
+                checkBackForward();
             }
         );
     };
